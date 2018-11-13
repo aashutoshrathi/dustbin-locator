286757287f71fd5d693421babc2d3ec11e0d085e
aashutoshrathi <aashutoshrathi@gmail.com>
2018-11-14 03:05:59 +0530

    Added auto-get GeoLocation

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function initMap() {
 	});
 
 	var infowindow = new google.maps.InfoWindow({});
+	var infoWindow = new google.maps.InfoWindow({});
+
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		  var pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		  };
+
+		  infoWindow.setPosition(pos);
+		  infoWindow.setContent('Location found.');
+		  infoWindow.open(map);
+		  map.setCenter(pos);
+		}, function() {
+		  handleLocationError(true, infoWindow, map.getCenter());
+		});
+	  } else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	  }
+
 	var marker;
 
 	var config = {
@@ -47,3 +69,12 @@ function initMap() {
 		});
 	}
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+						  'Error: The Geolocation service failed.' :
+						  'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
+  }
+
